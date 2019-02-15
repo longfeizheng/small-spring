a1b02357c42f0d267a3053e0a2a5b46396f71390
zhenglongfei <zhenglongfei@renrenche.com>
2019-02-15 16:14:26 +0800

    :tada: aop init

---
+++ src/test/java/com/niocoder/tx/TransactionManager.java
@@ -0,0 +1,28 @@
+package com.niocoder.tx;
+
+import com.niocoder.util.MessageTracker;
+import org.junit.Before;
+
+/**
+ * 用于测试AOP顺序
+ */
+public class TransactionManager {
+    @Before
+    public void setUp() {
+        MessageTracker.clearMsgs();
+    }
+
+    public void start() {
+        System.out.println("start tx");
+        MessageTracker.addMsg("start tx");
+    }
+
+    public void commit() {
+        System.out.println("commit tx");
+        MessageTracker.addMsg("commit tx");
+    }
+
+    public void rollback() {
+        System.out.println("rollback tx");
+    }
+}
